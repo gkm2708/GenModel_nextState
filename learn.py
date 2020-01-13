6b8cbd662534aded47b29dab9b7f0b18e6758e50
Gaurav Kumar <gkumar@techfak.uni-bielefeld.de>
2020-01-13 19:22:05 +0100

    new buffer schedule, changed inputs etc

--- learn.py
+++ learn.py
@@ -1,11 +1,16 @@
 # first neural network with keras tutorial
+import numpy as np
+import random
 from keras.layers import Dense, Input
 from keras.models import Model
 from keras.models import model_from_json
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
-#from keras.callbacks import Callback
 import keras
+
+
+
+
 
 class LossHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
@@ -14,13 +19,19 @@ class LossHistory(keras.callbacks.Callback):
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
 
+
+
+
+
 class Learner:
 
-    def __init__(self):
+    def __init__(self, buffer):
 
+        # set buffer
+        self.buffer = buffer
         # creating model
         inputs = Input(shape=(4,))
-        dense1 = Dense(256, activation='relu')(inputs)
+        dense1 = Dense(128, activation='relu')(inputs)
         dense2 = Dense(128, activation='relu')(dense1)
         #dense3 = Dense(128, activation='relu')(dense2)
 
@@ -35,35 +46,36 @@ class Learner:
         self.history = LossHistory()
         self.data_dump = []
 
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
-    def learn_step(self, x_data, y_data):
+    #def learn_step(self, x_data, y_data):
+    def learn_step(self):
 
-        self.model.fit(x_data, y_data, callbacks=[self.history])
-        #print(self.history.losses)
+        batch = self.buffer.sample(32)
+
+        self.model.fit(batch[0], batch[1], callbacks=[self.history], batch_size=32)
         # summarize history for accuracy
         if len(self.history.losses) > 0:
-            print(self.history.losses[0])
             self.data_dump.append(self.history.losses[0])
 
 
     def plot_graph(self):
-        print("save loss")
+        #print("save loss")
         plt.plot(range(len(self.data_dump)), self.data_dump)
         plt.title('model losses')
         plt.ylabel('loss')
         plt.xlabel('epoch')
         plt.savefig('books_read.png')
-        #plt.close()
-
 
 
     def evaluate(self, x_data, y_data):
         _, accuracy = self.model.evaluate(x_data, y_data)
         print('Accuracy: %.2f' % (accuracy*100))
 
+
     def predict(self, x_data):
         return self.model.predict(x_data)
+
 
     def save_model(self):
         model_json = self.model.to_json()
@@ -72,6 +84,7 @@ class Learner:
         # serialize weights to HDF5
         self.model.save_weights("model.h5")
         print("Saved model to disk")
+
 
     def load_model(self):
         # load json and create model
@@ -82,3 +95,67 @@ class Learner:
         # load weights into new model
         self.model.load_weights("model.h5")
         print("Loaded model from disk")
+
+
+
+
+
+
+
+
+class ReplayBuffer(object):
+    def __init__(self, size):
+        """Create Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        """
+        self._storage = []
+        self._maxsize = size
+        self._next_idx = 0
+
+    def __len__(self):
+        return len(self._storage)
+
+    def push(self, inp, label):
+        data = (inp, label)
+
+        if self._next_idx >= len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+
+    def _encode_sample(self, idxes):
+        inp_t, label_t = [], []
+        for i in idxes:
+            data = self._storage[i]
+            inp, label = data
+            inp_t.append(np.array(inp, copy=False))
+            label_t.append(np.array(label, copy=False))
+        return np.array(inp_t), np.array(label_t)
+
+    def sample(self, batch_size):
+        """Sample a batch of experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        """
+        idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
+        return self._encode_sample(idxes)
--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import os
-from tqdm import trange
-from gym_unity.envs.unity_env import UnityEnv
 import numpy as np
-from numpy import genfromtxt
-
-from learn import Learner
-import matplotlib.pyplot as plt
-
 import random
-import time
-np.set_printoptions(threshold=np.inf)
-
 import math
 
+from gym_unity.envs.unity_env import UnityEnv
+from learn import Learner
+from learn import ReplayBuffer
+
+np.set_printoptions(threshold=np.inf)
 
 # create grid and write to file
-maze_trials, max_episode, max_steps = 1, 1000000, 2
+maze_trials, max_episode, max_steps = 1, 100000, 100
 
-learner = Learner()
+buffer = ReplayBuffer(10000)
+learner = Learner(buffer)
 # assume that unity reads that file and generates maze dynamically
-env = UnityEnv("/home/gaurav/MySharedRepository/mazeContinuousTarget_fixed_camera/Build/mazeContinuousTarget",
+env = UnityEnv("/homes/gkumar/Documents/UnityProjects/mazeContinuousTarget_fixed_camera/Build/mazeContinuousTarget_fixed_camera_blank_board",
                0,
                use_visual=True,
                uint8_visual=True)
 
 label = np.zeros((9, 9))
 label[3:6, 3:6] = 1/9
-label = np.expand_dims(label.flatten(), axis=0)
+#label = np.expand_dims(label.flatten(), axis=0)
+label = label.flatten()
 
 
 def runEpisodeContinuous():
@@ -34,11 +31,15 @@ def runEpisodeContinuous():
     obs_fovea = env.reset()
     obs_fovea_next, cummulativeFovealReward, global_done, info = env.step([[0.0], [0.0]])
 
-    pos_x_prev = info["brain_info"].vector_observations[0][2]
-    pos_y_prev = info["brain_info"].vector_observations[0][3]
+    x_pos_new = info["brain_info"].vector_observations[0][2]
+    y_pos_new = info["brain_info"].vector_observations[0][3]
+    x_dot_new = info["brain_info"].vector_observations[0][0]
+    y_dot_new = info["brain_info"].vector_observations[0][1]
 
-    x_dot_prev = info["brain_info"].vector_observations[0][0]
-    y_dot_prev = info["brain_info"].vector_observations[0][1]
+    x_pos_prev = x_pos_new
+    y_pos_prev = y_pos_new
+    x_dot_prev = x_dot_new
+    y_dot_prev = y_dot_new
 
     for i in range(max_steps):
 
@@ -46,75 +47,75 @@ def runEpisodeContinuous():
         act_x = random.randrange(-100, 100)/100
         act_y = random.randrange(-100, 100)/100
 
+        x_vel = x_pos_new - x_pos_prev
+        y_vel = y_pos_new - y_pos_prev
+
+        x_pos_prev = x_pos_new
+        y_pos_prev = y_pos_new
+        x_dot_prev = x_dot_new
+        y_dot_prev = y_dot_new
+
         obs_fovea_next, cummulativeFovealReward, global_done, info = env.step([[act_x/10], [act_y/10]])
 
-        pos_x_new = info["brain_info"].vector_observations[0][2]
-        pos_y_new = info["brain_info"].vector_observations[0][3]
-
+        x_pos_new = info["brain_info"].vector_observations[0][2]
+        y_pos_new = info["brain_info"].vector_observations[0][3]
         x_dot_new = info["brain_info"].vector_observations[0][0]
         y_dot_new = info["brain_info"].vector_observations[0][1]
 
-        #print([x_dot_prev, x_dot_new, y_dot_prev, y_dot_new, x_dot_prev, x_dot_new, y_dot_prev, y_dot_new])
-        #print([x_dot_prev, y_dot_prev, x_dot_new, y_dot_new, pos_x_prev, pos_y_prev, pos_x_new, pos_y_new])
-
-        # derive a view of movement
+        # derive the direction of movement
         true_label = np.zeros((9,9))
-
-        #y = (y_dot_new - y_dot_prev)*(1 - x_dot_new)/(x_dot_new - x_dot_prev) + y_dot_new
-        theta = math.atan((y_dot_prev - y_dot_new) / (x_dot_prev - x_dot_new))
-
-        #print(theta)
-
-        if x_dot_new - x_dot_prev >= 0:
+        theta = math.atan((y_pos_prev - y_pos_new) / (x_pos_prev - x_pos_new))
+        if x_pos_new - x_pos_prev >= 0:
             if theta > -1.57 and theta < -1.18:
-                #print("0, -1")
                 true_label[4,3] = 1
-            if theta > -1.18 and theta < -0.39:
-                #print("1, -1")
+            elif theta > -1.18 and theta < -0.39:
                 true_label[5, 3] = 1
-            if theta > -0.39 and theta < 0.39:
-                #print("1, 0")
+            elif theta > -0.39 and theta < 0.39:
                 true_label[5, 4] = 1
-            if theta > 0.39 and theta < 1.18:
-                #print("1, 1")
+            elif theta > 0.39 and theta < 1.18:
                 true_label[5, 5] = 1
-            if theta > 1.18 and theta < 1.57:
-                #print("0, 1")
+            elif theta > 1.18 and theta < 1.57:
                 true_label[4,5] = 1
-        elif x_dot_new - x_dot_prev <= 0:
+        elif x_pos_new - x_pos_prev <= 0:
             if theta > -1.57 and theta < -1.18:
-                #print("0, -1")
                 true_label[4, 3] = 1
-            if theta > -1.18 and theta < -0.39:
-                #print("-1, -1")
+            elif theta > -1.18 and theta < -0.39:
                 true_label[3, 3] = 1
-            if theta > -0.39 and theta < 0.39:
-                #print("-1, 0")
+            elif theta > -0.39 and theta < 0.39:
                 true_label[3, 4] = 1
-            if theta > 0.39 and theta < 1.18:
-                #print("-1, 1")
+            elif theta > 0.39 and theta < 1.18:
                 true_label[3, 5] = 1
-            if theta > 1.18 and theta < 1.57:
-                #print("-1, 0")
+            elif theta > 1.18 and theta < 1.57:
                 true_label[3,4] = 1
 
-        true_label = np.expand_dims(true_label.flatten(), axis=0)
+        #true_label = np.expand_dims(true_label.flatten(), axis=0)
+        true_label = true_label.flatten()
+
+        print([x_vel, y_vel, x_dot_prev-x_dot_new, y_dot_prev-y_dot_new])
+
+
 
         # learn here
-        data = np.expand_dims(np.array([x_dot_prev, y_dot_prev, x_dot_prev-x_dot_new, y_dot_prev-y_dot_new], dtype=np.float32), axis=0)
-        learner.learn_step(data, true_label)
+        #data = np.expand_dims(np.array([x_dot_prev, y_dot_prev, x_dot_prev-x_dot_new, y_dot_prev-y_dot_new], dtype=np.float32), axis=0)
+        data = np.array([x_vel, y_vel, x_dot_prev-x_dot_new, y_dot_prev-y_dot_new], dtype=np.float32)
+        #learner.learn_step(data, true_label)
+        # save data in replay buffer
+        buffer.push(data, true_label)
 
         # learn here
-        data = np.expand_dims(np.array([x_dot_prev, y_dot_prev, 0, 0], dtype=np.float32), axis=0)
-        learner.learn_step(data, label)
+        #data = np.expand_dims(np.array([x_dot_prev, y_dot_prev, 0, 0], dtype=np.float32), axis=0)
+        #data = np.array([x_dot_prev, y_dot_prev, 0, 0], dtype=np.float32)
+        #learner.learn_step(data, label)
+        # save data in replay buffer
+        #buffer.push(data, label)
 
-        obs_fovea = obs_fovea_next
 
-        pos_x_prev = pos_x_new
-        pos_y_prev = pos_y_new
+        if buffer.__len__() > 100:
+            learner.learn_step()
 
-        x_dot_prev = x_dot_new
-        y_dot_prev = y_dot_new
+
+        #obs_fovea = obs_fovea_next
+
 
 
 if __name__ == '__main__':
@@ -130,23 +131,22 @@ if __name__ == '__main__':
 
         if i % 1000 == 0:
 
-            #print(learner.predict(np.expand_dims(np.array([1, 1, 0, 0], dtype=np.float32), axis=0)))
-
-            plt.plot(learner.predict(np.expand_dims(np.array([1, 1, 0, 0], dtype=np.float32), axis=0)))
-            plt.savefig('output'+str(i)+'_'+str(1)+'.png')
-
-            plt.plot(learner.predict(np.expand_dims(np.array([1, -1, 0, 0], dtype=np.float32), axis=0)))
-            plt.savefig('output'+str(i)+'_'+str(1)+'.png')
-
-            plt.plot(learner.predict(np.expand_dims(np.array([-1, 1, 0, 0], dtype=np.float32), axis=0)))
-            plt.savefig('output'+str(i)+'_'+str(1)+'.png')
-
-            plt.plot(learner.predict(np.expand_dims(np.array([-1, -1, 0, 0], dtype=np.float32), axis=0)))
-            plt.savefig('output'+str(i)+'_'+str(1)+'.png')
-
-            #print(learner.predict(np.expand_dims(np.array([1, -1, 0, 0], dtype=np.float32), axis=0)))
-            #print(learner.predict(np.expand_dims(np.array([-1, 1, 0, 0], dtype=np.float32), axis=0)))
-            #print(learner.predict(np.expand_dims(np.array([-1, -1, 0, 0], dtype=np.float32), axis=0)))
+            #plt.plot(learner.predict(np.expand_dims(np.array([1, 1, 0, 0], dtype=np.float32), axis=0)))
+            #plt.savefig('output'+str(i)+'_'+str(1)+'.png')
+            #plt.close()
+            #plt.plot(learner.predict(np.expand_dims(np.array([1, -1, 0, 0], dtype=np.float32), axis=0)))
+            #plt.savefig('output'+str(i)+'_'+str(1)+'.png')
+            #plt.close()
+            #plt.plot(learner.predict(np.expand_dims(np.array([-1, 1, 0, 0], dtype=np.float32), axis=0)))
+            #plt.savefig('output'+str(i)+'_'+str(1)+'.png')
+            #plt.close()
+            #plt.plot(learner.predict(np.expand_dims(np.array([-1, -1, 0, 0], dtype=np.float32), axis=0)))
+            #plt.savefig('output'+str(i)+'_'+str(1)+'.png')
+            #plt.close()
+            print(learner.predict(np.expand_dims(np.array([1, 1, 0, 0], dtype=np.float32), axis=0)))
+            print(learner.predict(np.expand_dims(np.array([1, -1, 0, 0], dtype=np.float32), axis=0)))
+            print(learner.predict(np.expand_dims(np.array([-1, 1, 0, 0], dtype=np.float32), axis=0)))
+            print(learner.predict(np.expand_dims(np.array([-1, -1, 0, 0], dtype=np.float32), axis=0)))
 
     # save model
     learner.save_model()
